1eaa0c0ee66afe441b8389ed95592338d68a81f0
ErikaStott <erika.r.stott@gmail.com>
2018-08-02 12:51:55 -0700

    first commit

---
+++ app/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+import Calculator from './components/Calculator/Calculator'
+
+class App extends React.Component {
+  render() {
+    return (Calculator
+      );
+  }
+}
+
+export default App;
